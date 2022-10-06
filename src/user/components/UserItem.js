784792b451d034f046645cf32a362f231bcc87a1
Perry <perryy41996@yahoo.com>
2022-10-06 01:54:59 -0400

    added avatar and card components

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./UserItem.css";
 import { Link } from "react-router-dom";
 import Avatar from "../components/UIElements/Avatar";
+import Card from "./UIElements/Card";
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
+      <Card className={"user-item__content"}>
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
             <Avatar image={props.image}></Avatar>
@@ -14,11 +15,11 @@ const UserItem = (props) => {
           <div className="user-item__info">
             <h2>{props.name}</h2>
             <h3>
-              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}{" "}
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
             </h3>
           </div>
         </Link>
-      </div>
+      </Card>
     </li>
   );
 };
