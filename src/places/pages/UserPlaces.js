68ac5f72b33dfb37f8a9674a76e44eeb2f8ec470
Perry <perryy41996@yahoo.com>
2022-10-08 07:03:55 -0400

    added PlaceList and PlaceItem components, also added dummy place data in new UserPlaces page to render for now

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,5 +1,23 @@
 import React from "react";
+import PlaceList from "../../user/components/PlaceList";
 
-const UserPlaces = () => {};
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "empire state building",
+    description: "One of the most famous buildings in the world.",
+    imageURL:
+      "https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU3ODc3NjU2NzUxNTgwODk1/this-day-in-history-05011931---empire-state-building-dedicated.jpg",
+    address: "20 W 34th St., New York, NY 10001",
+    location: {
+      lat: 40.7484445,
+      lng: -73.9856644,
+    },
+  },
+];
+
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
 
 export default UserPlaces;
