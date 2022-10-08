5dfd9fd42bf477d14dfbee5f087bfb576446cee3
Perry <perryy41996@yahoo.com>
2022-10-08 03:32:30 -0400

    added side drawer portal

--- src/user/components/Navigation/MainNavigation.js
+++ src/user/components/Navigation/MainNavigation.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Fragment } from "react";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
 function MainNavigation(props) {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavLinks></NavLinks>
-      </nav>
-    </MainHeader>
+    <Fragment>
+      {
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      }
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks></NavLinks>
+        </nav>
+      </MainHeader>
+    </Fragment>
   );
 }
 
