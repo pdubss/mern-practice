178dbb02d6d86f00e96a3097ddae72816b8eabea
Perry <perryy41996@yahoo.com>
2022-10-08 03:04:34 -0400

    added sidebar component

--- src/user/components/Navigation/MainNavigation.js
+++ src/user/components/Navigation/MainNavigation.js
@@ -16,7 +16,7 @@ function MainNavigation(props) {
       <h1 className="main-navigation__title">
         <Link to="/">Your Places</Link>
       </h1>
-      <nav>
+      <nav className="main-navigation__header-nav">
         <NavLinks></NavLinks>
       </nav>
     </MainHeader>
