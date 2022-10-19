0a80f23cc70bc60163a38046c83682fb37ad7670
Perry <perryy41996@yahoo.com>
2022-10-18 20:44:46 -0400

    added useForm custom hook

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,42 +1,17 @@
-import React, { useCallback, useReducer } from "react";
-
-import "./PlaceForm.css";
+import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import {
-  VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
-} from "../../shared/components/FormElements/util/validators";
-
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case "INPUT_CHANGE":
-      let formIsValid = true;
-      for (const input in state.inputs) {
-        if (input === action.input) {
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[input].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.input]: { value: action.value, isValid: action.isValid },
-        },
-        isValid: formIsValid,
-      };
-
-    default:
-      return state;
-  }
-};
+  VALIDATOR_MINLENGTH,
+} from "../../shared/components/FormElements/util/validators.js";
+import { useForm } from "../../shared/hooks/form-hook";
+import "./PlaceForm.css";
 
 const NewPlace = () => {
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
+  const [formState, inputHandler] = useForm(
+    {
       title: {
         value: "",
         isValid: false,
@@ -50,47 +25,40 @@ const NewPlace = () => {
         isValid: false,
       },
     },
-  });
-  const InputHandler = useCallback((id, value, isValid) => {
-    dispatch({
-      type: "INPUT_CHANGE",
-      value: value,
-      isValid: isValid,
-      input: id,
-    });
-  }, []);
+    false
+  );
 
   const placeSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
+    console.log(formState.inputs); // send this to the backend!
   };
 
   return (
     <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
-        id="id"
+        id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
-        onInput={InputHandler}
-        errorText={"Please enter a valid title"}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
       />
       <Input
         id="description"
         element="textarea"
         label="Description"
-        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
-        onInput={InputHandler}
-        errorText={"Description must be at least 5 characters"}
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
       />
       <Input
         id="address"
         element="input"
         label="Address"
-        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
-        onInput={InputHandler}
-        errorText={"Please enter a valid address"}
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
+        onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
         ADD PLACE
